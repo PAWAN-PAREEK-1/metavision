4ca0f2d46407875477c0a3d86f9d09abb84a1e07
PAWAN-PAREEK-1 <pawapareek506@gmail.com>
2023-08-14 19:30:22 +0530

    hero

--- metavision/src/Pages/Home.js
+++ metavision/src/Pages/Home.js
@@ -1,9 +1,78 @@
-import React from 'react'
+import React from "react";
+import { lazy, Suspense } from "react";
+import line from "../assets/img/line.svg";
+import play from "../assets/img/play.svg";
+import women from "../assets/img/women.png";
+import "../Style/Home.css";
+import chart from "../assets/img/chart.svg";
+const Loading = lazy(() => import("../Components/Loading"));
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <Suspense
+      fallback={
+        <div>
+          <Loading />
+        </div>
+      }
+    >
+      <div>
+        <div className="main">
+          <section className="hero">
+            <div className="hero-detail">
+              <h2>
+                Welcome To Metavision <img src={line} alt="" />
+              </h2>
+              <h1>Data will talk to you if you're willing to listen.</h1>
+              <h3>
+                Easy, Dynamic, Flexible, Accurate, and Quick reporting with F-1
+                MIS.
+              </h3>
+              <div className="hero-work">
+                <a href="">
+                  <div className="btn">GET Estimate</div>
+                </a>
 
-export default Home
+                <div className="hero-work-2">
+                  <a href="">
+                    {" "}
+                    <img src={play} alt="" />
+                  </a>
+                  <h4>See How It works</h4>
+                  <img src={line} alt="" />
+                </div>
+              </div>
+            </div>
+            <div className="heroimg">
+              <img src={women} alt="" />
+              {/* <div className="monthly">
+              <img src={chart} alt="" />
+
+            </div> */}
+            </div>
+          </section>
+          <div className="exp">
+            <div className="exp-box">
+              <h1>11+</h1>
+              <h2>YEARS OF EXPERIENCE</h2>
+            </div>
+            <div className="exp-box">
+              <h1>2000+</h1>
+              <h2>SATISFIED CLIENTS</h2>
+            </div>
+            <div className="exp-box">
+              <h1>25000+</h1>
+              <h2>TOTAL USERS</h2>
+            </div>
+            <div className="exp-box">
+              <h1>1+</h1>
+              <h2>COUNTRIES</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Suspense>
+  );
+};
+
+export default Home;
