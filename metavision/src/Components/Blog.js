6f0efe05722eac4748d477e36e0d7bd8be8b2053
PAWAN-PAREEK-1 <pawapareek506@gmail.com>
2023-08-15 21:51:34 +0530

    blog section update

--- metavision/src/Components/Blog.js
+++ metavision/src/Components/Blog.js
@@ -6,7 +6,57 @@ import blog2 from "../assets/img/b2.svg";
 import blog3 from "../assets/img/b3.svg";
 import blog4 from "../assets/img/blog1.svg";
 
+const blogData = [
+    {
+      image: blog1,
+      title: "The Most Popular Business Of the Year",
+      content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley",
+      authorImage: blog4,
+      authorName: "Dulce Korsgaard",
+      authorRole: "UI/UX Designer",
+      date: "July 20, 2020",
+    },
+    {
+        image: blog2,
+        title: "The Most Popular Business Of the Year",
+        content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley",
+        authorImage: blog4,
+        authorName: "Martin Donin",
+        authorRole: "Consulting",
+        date: "July 20, 2020",
+      },
+      {
+        image: blog3,
+        title: "The Most Popular Business Of the Year",
+        content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley",
+        authorImage: blog4,
+        authorName: "Rayna Korsgaard",
+        authorRole: "User Research",
+        date: "July 20, 2020",
+      },
+      {
+        image: blog1,
+        title: "The Most Popular Business Of the Year",
+        content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley",
+        authorImage: blog4,
+        authorName: "Dulce Korsgaard",
+        authorRole: "UI/UX Designer",
+        date: "July 20, 2020",
+      },
+
+
+  ];
 const Blog = () => {
+  const itemsPerGroup = 3;
+  const totalGroups = Math.ceil(blogData.length / itemsPerGroup);
+
+  const [activeGroup, setActiveGroup] = useState(0);
+
+  const visibleBlogs = blogData.slice(
+    activeGroup * itemsPerGroup,
+    (activeGroup + 1) * itemsPerGroup
+  );
+
   return (
     <div>
       <div className="product serv  blogs">
@@ -18,30 +68,40 @@ const Blog = () => {
           since the 1500s, when an unknown printer took a galley
         </p>
 
-        <div className="service-box blog">
-          <div className="blog-img">
-            <img src={blog1} alt="" />
-          </div>
-          <div className="blog-detail">
-            <h2>The most Popular Business Of the Year</h2>
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley
-            </p>
-          </div>
-          <div className="blog-name">
-            <div className="name-img">
-              <div className="blog-p-img">
-                <img src={blog4} alt="" />
+        <div className="blog-box">
+          {visibleBlogs.map((blog, index) => (
+            <div className="service-box blog" key={index}>
+              <div className="blog-img">
+                <img src={blog.image} alt="" />
               </div>
-              <div className="blog-p-name">
-                <h3>Dulce Korsgaard</h3>
-                <h5>UI/UX Designer</h5>
+              <div className="blog-detail">
+                <h2>{blog.title}</h2>
+                <p>{blog.content}</p>
+              </div>
+              <div className="blog-name">
+                <div className="name-img">
+                  <div className="blog-p-img">
+                    <img src={blog.authorImage} alt="" />
+                  </div>
+                  <div className="blog-p-name">
+                    <h3>{blog.authorName}</h3>
+                    <h5>{blog.authorRole}</h5>
+                  </div>
+                </div>
+                <h2>{blog.date}</h2>
               </div>
             </div>
-            <h2>July 20, 2020</h2>
-          </div>
+          ))}
+        </div>
+
+        <div className="circel">
+          {Array.from({ length: totalGroups }, (_, i) => (
+            <span
+              className={activeGroup === i ? "active" : ""}
+              onClick={() => setActiveGroup(i)}
+              key={i}
+            ></span>
+          ))}
         </div>
       </div>
     </div>
@@ -49,3 +109,123 @@ const Blog = () => {
 };
 
 export default Blog;
+
+
+
+
+
+
+
+// import React, { useState } from "react";
+// import "../Style/Services.css";
+// import "../Style/Global.css";
+// import blog1 from "../assets/img/b1.svg";
+// import blog2 from "../assets/img/b2.svg";
+// import blog3 from "../assets/img/b3.svg";
+// import blog4 from "../assets/img/blog1.svg";
+
+// const blogData = [
+//     {
+//         image: blog1,
+//         title: "The Most Popular Business Of the Year",
+//         content: "Lorem Ipsum is simply dummy text...",
+//         authorImage: blog4,
+//         authorName: "Dulce Korsgaard",
+//         authorRole: "UI/UX Designer",
+//         date: "July 20, 2020",
+//       },
+//       {
+//         image: blog1,
+//         title: "The Most Popular Business Of the Year",
+//         content: "Lorem Ipsum is simply dummy text...",
+//         authorImage: blog4,
+//         authorName: "Dulce Korsgaard",
+//         authorRole: "UI/UX Designer",
+//         date: "July 20, 2020",
+//       },
+//       {
+//         image: blog1,
+//         title: "The Most Popular Business Of the Year",
+//         content: "Lorem Ipsum is simply dummy text...",
+//         authorImage: blog4,
+//         authorName: "Dulce Korsgaard",
+//         authorRole: "UI/UX Designer",
+//         date: "July 20, 2020",
+//       },
+//       {
+//         image: blog1,
+//         title: "The Most Popular Business Of the Year",
+//         content: "Lorem Ipsum is simply dummy text...",
+//         authorImage: blog4,
+//         authorName: "Dulce Korsgaard",
+//         authorRole: "UI/UX Designer",
+//         date: "July 20, 2020",
+//       },
+// ];
+
+// const Blog = () => {
+//   const itemsPerGroup = 3;
+//   const totalGroups = Math.ceil(blogData.length / itemsPerGroup);
+
+//   const [activeGroup, setActiveGroup] = useState(0);
+
+//   const visibleBlogs = blogData.slice(
+//     activeGroup * itemsPerGroup,
+//     (activeGroup + 1) * itemsPerGroup
+//   );
+
+//   return (
+//     <div>
+//       <div className="product serv  blogs">
+//         <h3>/ Our Blog /</h3>
+//         <h1>Blog</h1>
+//         <p>
+//           Lorem Ipsum is simply dummy text of the printing and typesetting
+//           industry. Lorem Ipsum has been the industry's standard dummy text ever
+//           since the 1500s, when an unknown printer took a galley
+//         </p>
+
+//         <div className="service-box blog">
+//             <div className="blog-detailss">
+//           {visibleBlogs.map((blog, index) => (
+//             <div className="blog-entry" key={index}>
+//               <div className="blog-img">
+//                 <img src={blog.image} alt="" />
+//               </div>
+//               <div className="blog-detail">
+//                 <h2>{blog.title}</h2>
+//                 <p>{blog.content}</p>
+//               </div>
+//               <div className="blog-name">
+//                 <div className="name-img">
+//                   <div className="blog-p-img">
+//                     <img src={blog.authorImage} alt="" />
+//                   </div>
+//                   <div className="blog-p-name">
+//                     <h3>{blog.authorName}</h3>
+//                     <h5>{blog.authorRole}</h5>
+//                   </div>
+//                 </div>
+//                 <h2>{blog.date}</h2>
+//               </div>
+//             </div>
+//           ))}
+//         </div>
+//         </div>
+
+//         <div className="circel">
+//           {Array.from({ length: totalGroups }, (_, i) => (
+//             <span
+//               className={activeGroup === i ? "active" : ""}
+//               onClick={() => setActiveGroup(i)}
+//               key={i}
+//             ></span>
+//           ))}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Blog;
+
