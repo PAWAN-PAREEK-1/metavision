8f5a9cc0f3ca2c8fdeb1f9a426f01e928fa87d30
PAWAN-PAREEK-1 <pawapareek506@gmail.com>
2023-08-16 18:11:55 +0530

    about page

--- metavision/src/Pages/About.js
+++ metavision/src/Pages/About.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { lazy, Suspense } from "react";
+import "../Style/Global.css";
+import bigline from "../assets/img/bigline.svg";
+import herobg from "../assets/img/herobg.svg";
+const Loading = lazy(() => import("../Components/Loading"));
+const AboutUs = lazy(() => import("../Components/AboutUs"));
+
+const Blog = lazy(() => import("../Components/Blog"));
+const MetaVision = lazy(() => import("../Components/MetaVision"));
+const Testimonial = lazy(() => import("../Components/Testimonial"));
+const Contact = lazy(() => import("../Components/Contact"));
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <Suspense
+      fallback={
+        <div>
+          <Loading />
+        </div>
+      }
+    >
+      <div>
+        <div className="hero-all">
+          <h4>
+            {" "}
+            <img src={bigline} alt="" />
+            About Us
+          </h4>
+          <img src={herobg} alt="" />
+        </div>
 
-export default About
+        <AboutUs/>
+        <Blog/>
+        <MetaVision/>
+        <Testimonial/>
+        <Contact/>
+      </div>
+    </Suspense>
+  );
+};
+
+export default About;
